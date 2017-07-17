f3070275bc4e7e958c2294616c5c030ec80197a8
Michał <brakowski.m@gmail.com>
2017-07-17 19:30:30 +0200

    changes to the html, css and js

--- javascript/main.js
+++ javascript/main.js
@@ -3,20 +3,7 @@ $(function() {
     
     function slideToggle() {
         
-    var singleSlide = $(".mobile-sliders-container").find(".slides-animating-item");
-    console.log(singleSlide);
-    console.log(typeof(singleSlide));
     
-    singleSlide.each(function() {
-        
-        if($(this).css(("display") == "none")) {
-            $(this).next().css(("display") == "block");
-        } else {
-            $(this).next().css(("display") == "none");
-        }
-    });
-        
-    setInterval(slideToggle, 7000);
 }
 
 slideToggle();
