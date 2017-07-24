98c07bb8fdb92c49260b1f1add736364d2dfb643
Michał <brakowski.m@gmail.com>
2017-07-24 23:03:17 +0200

    changes to the css styles, the html structure and the JS

--- javascript/main.js
+++ javascript/main.js
@@ -3,19 +3,26 @@ $(function() {
     
     var prevBtn = $(".slider-arrow-left");
     var nextBtn = $(".slider-arrow-right");
+    var currentImg = $("#active");
+    var nextImg = $(".slider-item-wrapper").not("[id='active']");
     console.log(prevBtn,nextBtn);
+    console.log(currentImg,nextImg);
     
     nextBtn.on("click", function(e) {
-        var currentImg = $(".active");
-        var nextImg = $(".slider-arrow-wrapper:last-child").find("img:not(.active)");
-        
         e.stopImmediatePropagation();
         
-        if(nextImg.length) {
-            currentImg.removeClass("active").css("z-index", -10);
-            nextImg.addClass("active").css("z-index", 10);
+        if(currentImg.attr("id", "active")) {
+            currentImg.removeAttr("id").css("display", "none");
+            nextImg.attr("id", "active").css("display", "block");
         }
     });
-
-});
     
+    prevBtn.on("click", function(e) {
+        e.stopImmediatePropagation();
+        
+        if(currentImg.attr("id", "active")) {
+            currentImg.removeAttr("id").css("display", "none");
+            nextImg.attr("id", "active").css("display", "block");
+        }
+    });
+});
